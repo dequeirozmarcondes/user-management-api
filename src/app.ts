8841424e925635df6141e7a0bed450300771c453
dequeirozmarcondes <dequeirozmarcondes@outlook.com>
2025-03-06 00:43:07 -0400

    swagger setup

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 //app.ts
 import express from "express";
 import { router } from "./presentation/routes/userRoutes";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json"; // Adjust the path as necessary
+
 
 
 const app = express();
+
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.use(router);
 
