229c718077ad0f1c11cd1c7e3a20d53e07b7b703
dequeirozmarcondes <dequeirozmarcondes@outlook.com>
2025-03-07 00:20:31 -0400

    hash password

--- src/application/use-cases/users/CreateUserUseCase.ts
+++ src/application/use-cases/users/CreateUserUseCase.ts
@@ -1,7 +1,6 @@
-//CreateUserUseCase.ts
-
 import { CreateUserDTO } from "../../dto/UserDTO";
 import { IUserRepository } from "../../interfaces/IUserRepository";
+import bcrypt from "bcryptjs"; // Certifique-se de instalar o bcryptjs no seu projeto
 
 export class CreateUserUseCase {
     private userRepository: IUserRepository;
@@ -11,8 +10,23 @@ export class CreateUserUseCase {
     }
 
     async execute(data: CreateUserDTO) {
-        const user = await this.userRepository.create(data); // Usando o repositório
+        // Validação da senha (pelo menos 8 caracteres e uma letra e um número)
+        const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+        if (!passwordRegex.test(data.password)) {
+            throw new Error(
+                "Password must be at least 8 characters long and contain at least one letter and one number",
+            );
+        }
+
+        // Criptografar a senha
+        const hashedPassword = await bcrypt.hash(data.password, 10);
+
+        // Criar o usuário com a senha criptografada
+        const user = await this.userRepository.create({
+            ...data,
+            password: hashedPassword,
+        });
+
         return user;
     }
 }
-
